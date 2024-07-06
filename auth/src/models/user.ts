012a3a01ff373886e4fb89e242a08518f8603f98
Sebastian <sebastian.juras@gmail.com>
2024-07-06 22:18:08 +0200

    User model for mongodb interaction

---
+++ auth/src/models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+// To get Typescript typechecking we do this instead of mongoose new User...
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs.email, attrs.password)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+
+
+
+export { User };
