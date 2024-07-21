f108c620c4b61aaa474e585c6a6ae0fd0ebef39d
sebastianjuras <sebastian.juras@gmail.com>
2024-07-21 22:08:59 +0200

    JWT cookie on prerender

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,16 @@
 import '../styles/globals.css'
-
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+import { buildClient } from 'src/api/build-client'
+import App from 'next/app'
+ 
+const AppComponent = ({Component, pageProps}) => {
+    return <Component {...pageProps}/>
 }
+AppComponent.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  console.log(Object.keys(appContext).toString())
+  const {data} = await buildClient(appContext.ctx).get('/api/users/currentuser')
+  console.log(data)
+  return { data,...appProps }
+}
+export default AppComponent;
